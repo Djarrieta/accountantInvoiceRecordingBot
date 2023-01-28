4136684524816e092cc3a88c6d194d2e9a3d5e3e
djarrieta27 <darrieta@atlassian.com>
2023-01-27 19:06:32 -0500

    add a test change

--- src/services/getUserData.py
+++ src/services/getUserData.py
@@ -7,4 +7,5 @@ def getUserData(state):
     load_dotenv()
     state.userData["telegramToken"] = os.getenv("TELEGRAM_TOKEN")
     state.userData["openaiToken"] = os.getenv("OPENAI_TOKEN")
+
     state.userData["chatId"] = os.getenv("CHAT_ID")
