a9c2e6336863266f315eb9f3170faf04d45caaae
djarrieta27 <darrieta@atlassian.com>
2023-02-04 20:46:44 -0500

    get chat id from telegram

--- src/services/getOpenAiResponse.py
+++ src/services/getOpenAiResponse.py
@@ -3,7 +3,7 @@ def getOpenAiResponse(state, input):
         raise Exception("OpenAI not mounted")
 
     response = state.openai.Completion.create(
-        model="text-davinci-003", prompt=input, temperature=0.7, max_tokens=20)
+        model="text-davinci-003", prompt=input, temperature=0.7, max_tokens=200)
 
     if response["choices"] is None:
         raise Exception("OpenAI response is empty")
--- src/services/mountTelegram.py
+++ src/services/mountTelegram.py
@@ -1,4 +1,6 @@
 import telebot
+from telebot import types
+
 from services.getOpenAiResponse import getOpenAiResponse
 
 
@@ -8,9 +10,19 @@ def mountTelegram(state):
 
     state.telegram = telebot.TeleBot(state.userData["telegramToken"])
 
+    markup = types.ReplyKeyboardMarkup(row_width=1)
+    markup.add(types.KeyboardButton('ChatId'))
+    markup.add(types.KeyboardButton('MaxLength'))
+
+    state.telegram.send_message(
+        state.userData["chatId"], "Ask whatever you want", reply_markup=markup)
+
     @state.telegram.message_handler(content_types=["text"])
     def bot_text_messages(message):
-        response = getOpenAiResponse(state, message.text)
-        state.telegram.send_message(message.chat.id, response)
+        if message.text == "ChatId":
+            state.telegram.send_message(message.chat.id, message.chat.id)
+        else:
+            response = getOpenAiResponse(state, message.text)
+            state.telegram.send_message(message.chat.id, response)
 
     state.telegram.infinity_polling()
